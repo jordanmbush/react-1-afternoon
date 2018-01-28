f4be3e9138690329e4b41008c0608009dc6639e4
unknown <jordanmbush@gmail.com>
2018-01-28 10:07:53 -0700

    completed step 3

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -4,15 +4,47 @@ export default class EvenAndOdd extends Component {
     constructor() {
         super();
         this.state = {
+            evenArray: [],
+            oddArray: [],
+            userInput: '',
         }
+
+        this.updateUserInput = this.updateUserInput.bind(this);
+        this.splitEvensAndOdds = this.splitEvensAndOdds.bind(this); 
     }
+
+    updateUserInput(value){
+        this.setState({userInput: value,});
+    }
+
+    splitEvensAndOdds(){
+        // Check if userInput is empty - exit if it is
+        // Null value is passed to oddArray if mixedArray is empty
+        if(this.state.userInput === '') return;
+        let mixedArray = this.state.userInput.split(",").map( e => parseInt(e));
+        let evenArray = mixedArray.filter( e => e % 2 === 0);
+        let oddArray = mixedArray.filter( e => e % 2 !== 0);
+
+        this.setState({
+            evenArray: evenArray,
+            oddArray: oddArray,
+            userInput: '',
+        });
+        
+    }
+
 
     render () {
+        console.log(this.state);
 
         return (
-            <p>EvenAndOdd Component</p>
+            <div className="puzzleBox evenAndOddPB">
+                <h4>Evens and Odds</h4>
+                <input className="inputLine" onChange={ (e) => this.updateUserInput(e.target.value)} value={this.state.userInput}></input>
+                <button className="confirmationButton" onClick={() => this.splitEvensAndOdds()}>Split</button>
+                <span className="resultsBox">Evens: {JSON.stringify(this.state.evenArray)}</span>
+                <span className="resultsBox">Odds: {JSON.stringify(this.state.oddArray)}</span>
+            </div>
         );
     }
-
-
 }
