0c2bef23b4f830390523a667e8d43c41459cd341
unknown <jordanmbush@gmail.com>
2018-01-28 20:04:46 -0700

    completed step 7

--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -4,13 +4,38 @@ export default class Sum extends Component {
     constructor() {
         super();
         this.state = {
+            num1: 0,
+            num2: 0,
+            sum: null
         }
+        this.updateNum1 = this.updateNum1.bind(this);
+        this.updateNum2 = this.updateNum2.bind(this);
+        this.updateSum = this.updateSum.bind(this);
     }
 
-    render () {
+    updateNum1(num){
+        this.setState({num1: parseInt(num, 10)});
+    }
 
+    updateNum2(num){
+        this.setState({num2: parseInt(num, 10)});
+    }
+
+    updateSum(){
+        let sum = this.state.num1 + this.state.num2;
+        this.setState({sum: sum});
+    }
+
+
+    render () {
         return (
-            <p>Sum Component</p>
+            <div className="puzzleBox sumPB">
+                <h4> Sum </h4>
+                <input className="inputLine" type="number" onChange={ (e) => this.updateNum1(e.target.value)}></input>
+                <input className="inputLine" type="number" onChange={ (e) => this.updateNum2(e.target.value)}></input>
+                <button className="confirmationButton" onClick={ () => this.updateSum()}>Add</button>
+                <span className="resultsBox">Sum: {this.state.sum}</span>
+            </div>
         );
     }
 
